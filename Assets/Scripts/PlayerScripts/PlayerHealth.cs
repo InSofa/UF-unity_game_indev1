f11737c4a28f239aaeeecbcde096f05b44196588
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2025-01-20 16:25:33 +0100

    Add debug-overlay
    + Added a debug-overlay that toggles on 'P'
    ~ Decided apon the Top-HP-Bar

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -11,7 +12,9 @@ public class PlayerHealth : MonoBehaviour
     private float maxHealth;
 
     [SerializeField]
-    Slider healthSlider, healthSlider2;
+    Slider healthSlider;
+
+    public bool canDie = true;
 
 
     public void Start()
@@ -20,18 +23,9 @@ public class PlayerHealth : MonoBehaviour
         healthSlider.maxValue = maxHealth;
         healthSlider.value = health;
         healthSlider.gameObject.SetActive(true);
-
-        healthSlider2.maxValue = maxHealth;
-        healthSlider2.value = health;
-        healthSlider2.gameObject.SetActive(false);
     }
 
     public void Update() {
-        if (Input.GetKeyUp(KeyCode.Q)) {
-            Debug.Log("switched");
-            healthSlider.gameObject.SetActive(!healthSlider.gameObject.activeSelf);
-            healthSlider2.gameObject.SetActive(!healthSlider2.gameObject.activeSelf);
-        }
     }
 
     public void TakeDamage(float damage, GameObject source)
@@ -39,10 +33,12 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log("took damage");
         health -= damage;
         healthSlider.value = health;
-        healthSlider2.value = health;
         if (health < 0)
         {
-            Destroy(this.gameObject);
+            if (canDie == true) {
+                //Destroy(this.gameObject);
+                SceneManager.LoadScene(0);
+            }
         }
     }
 }
