5c9d025dfd8fac58d6bb35e3048ebdfc1e43d2fb
bumblebroo <otis.gustafsson@hotmail.com>
2025-02-28 10:56:42 +0100

    started work on new turret

--- Assets/Scripts/Entities/Buildings/turretProjectile.cs
+++ Assets/Scripts/Entities/Buildings/turretProjectile.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TurretProjectile : MonoBehaviour
 {
     public float damage;
+
+    public bool isPiercing;
+
+    EnemyHealth[] enemiesHit;
 
     public LayerMask hitMask;
 
@@ -14,8 +19,16 @@ public class TurretProjectile : MonoBehaviour
         EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
         if (enemyHealth != null)
         {
-            enemyHealth.TakeDamage(damage);
-            Destroy(this.gameObject);
+            if (isPiercing) {
+                EnemyHealth check = enemiesHit.FirstOrDefault(e => e == enemyHealth);
+                if (check == null) {
+                    enemyHealth.TakeDamage(damage);
+                    enemiesHit.Append(enemyHealth);
+                }
+            } else {
+                enemyHealth.TakeDamage(damage);
+                Destroy(this.gameObject);
+            }
         }
 
     }
