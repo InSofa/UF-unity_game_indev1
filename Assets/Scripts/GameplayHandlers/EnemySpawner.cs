a4c28c4a47b8c387f7039683be01bee20eecd73e
bumblebroo <otis.gustafsson@hotmail.com>
2025-03-03 13:56:57 +0100

    minor changes to spawning

--- Assets/Scripts/GameplayHandlers/EnemySpawner.cs
+++ Assets/Scripts/GameplayHandlers/EnemySpawner.cs
@@ -7,10 +7,23 @@ public class EnemySpawner : MonoBehaviour
     public static EnemySpawner instance;
 
     //To keep track of all enemies currently alive
+    [HideInInspector]
     public List<GameObject> currentEnemies;
 
     [SerializeField]
     float minSpawnDist = 25f;
+
+    [SerializeField]
+    float wallRadius = 30f;
+
+    [SerializeField]
+    LineRenderer wall;
+
+    [SerializeField]
+    EdgeCollider2D wallCollider;
+
+    [SerializeField]
+    PolygonCollider2D wallCameraCollider;
 
     [SerializeField]
     GameObject enemy;
@@ -30,6 +43,7 @@ public class EnemySpawner : MonoBehaviour
     void Start()
     {
         instance = this;
+        GenerateWall();
         //SpawnWave();
     }
 
@@ -45,6 +59,27 @@ public class EnemySpawner : MonoBehaviour
         */
     }
 
+    /*
+    public void GenerateWall()
+    {
+        //Debug.Log($"Segment count: {segmentCount}, wallRadius: {wallRadius} SegmentSize: {segmentSize} Div: {segmentAngle}");
+        int segmentCount = 361;
+
+        wall.positionCount = segmentCount;
+
+        List<Vector3> points3D = new List<Vector3>();
+        List<Vector2> points = new List<Vector2>();
+        for (int i = 0; i < segmentCount; i++) {
+            float x = Mathf.Sin(Mathf.Deg2Rad * i) * wallRadius;
+            float y = Mathf.Cos(Mathf.Deg2Rad * i) * wallRadius;
+            points.Add(new Vector2(x, y));
+        }
+
+        wallCollider.points = points.ToArray();
+        //wallCameraCollider.SetPath(segmentCount, points.ToArray());
+        wall.SetPositions(points3D.ToArray());
+    }*/
+
     public void SpawnWave()
     {
         //Additional measures to make sure no new wave is started when a current one is still active
@@ -58,6 +93,18 @@ public class EnemySpawner : MonoBehaviour
         for (int i = 0; i < enemiesToSpawn; i++)
         {
             GameObject spawnObject = Instantiate(enemy, spawnPos, Quaternion.identity);
+        }
+    }
+
+    public void SpawnWaveAdvanced() {
+        int spawnPoints = 4;
+
+        for (int i = 0; i < spawnPoints; i++) {
+            float angle = Random.Range(0, 360);
+
+            float x = Mathf.Sin(Mathf.Deg2Rad * angle) * wallRadius;
+            float y = Mathf.Cos(Mathf.Deg2Rad * angle) * wallRadius;
+            //points.Add(new Vector2(x, y));
         }
     }
 }
