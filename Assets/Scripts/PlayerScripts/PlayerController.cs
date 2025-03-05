9d4a8b9208d5159c3b29da67cb3f5b3df4a116e1
bumblebroo <112719693+bumblebroo@users.noreply.github.com>
2025-03-05 20:34:44 +0100

    set upp animator
    - dont got all the sprites yet so not finished yet

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -43,16 +43,29 @@ public class PlayerController : MonoBehaviour
 
     float walkMagnitude;
 
+    [Header("Animation")]
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     private void Start() {
         player = this.gameObject;
         rb = GetComponent<Rigidbody2D>();
         lsc = GetComponent<LocalSoundComposer>();
+
+        Vector2 BetweenDU = new Vector2(Mathf.Cos(67.5f), Mathf.Sin(67.5f));
+        Vector2 BetweenS = new Vector2(Mathf.Cos(22.5f), Mathf.Sin(22.5f));
+
+        Debug.Log($"BetweenDU: {BetweenDU}, BetweenS: {BetweenS}");
     }
 
     // Update is called once per frame
     private void Update() {
         takeInput();
+        updateVisuals();
 
         if (walkMagnitude >= walkSoundInterval) {
             lsc.PlayRandomFx(walkSounds);
@@ -64,8 +77,51 @@ public class PlayerController : MonoBehaviour
         movementLogic();
     }
 
+    void updateVisuals() {
+        if (movementInput.magnitude > 0.1f) {
+            animator.SetInteger("State", 0);
+            return;
+        }
+
+        if (movementInput.x > 0) {
+            sr.flipX = true;
+        } else if (movementInput.x < 0) {
+            sr.flipX = false;
+        }
+
+        /*States
+         0 - Idle
+         1 - Up
+         2 - Side Up
+         3 - Side
+         4 - Side Down
+         5 - Down
+         */
+
+        switch (movementInput.x) {
+            case < 0.3826834f:
+                if (movementInput.y > 0) {
+                    animator.SetInteger("State", 1);
+                } else if (movementInput.y < 0) {
+                    animator.SetInteger("State", 5);
+                }
+                break;
+            case > 0.3826834f:
+                if (movementInput.y > 0.3826834f) {
+                    animator.SetInteger("State", 2);
+                } else if (movementInput.y < -0.3826834f) {
+                    animator.SetInteger("State", 4);
+                } else {
+                    animator.SetInteger("State", 3);
+                }
+                break;
+            default:
+                animator.SetInteger("State", 0);
+                break;
+        }
+    }
     void takeInput() {
-        movementInput = move.action.ReadValue<Vector2>();
+        movementInput = move.action.ReadValue<Vector2>().normalized;
     }
 
     void movementLogic() {
@@ -76,10 +132,6 @@ public class PlayerController : MonoBehaviour
         if(movementInput == Vector2.zero)
         {
             lerpFloatValue = deAccelerate;
-        }
-        else
-        {
-            movementInput.Normalize();
         }
 
         velocity = Vector2.Lerp(velocity, movementInput * speed, lerpFloatValue * Time.fixedDeltaTime);
