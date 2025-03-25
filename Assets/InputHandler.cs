0138c02105cc28547da5152a25ceaf0826f7cfa6
bumblebroo <112719693+bumblebroo@users.noreply.github.com>
2025-03-25 20:35:51 +0100

    minor edits - seems like everything is kinda fucked

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -2,7 +2,6 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using static UnityEditor.PlayerSettings;
 
 public class InputHandler : MonoBehaviour {
     private static InputHandler instance;
